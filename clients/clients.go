a635e93b55af9e6bbab7238619d1148857db1356
Saidamir Botirov <saidamir.botirov@gmail.com>
2022-12-01 18:22:12 +0500

    grpc Client init

---
+++ clients/clients.go
@@ -0,0 +1,32 @@
+package clients
+
+import (
+	"uacademy/blogpost/api_gateway/config"
+	"uacademy/blogpost/api_gateway/protogen/blogpost"
+
+	"google.golang.org/grpc"
+)
+
+type GrpcClients struct {
+	Author  blogpost.AuthorServiceClient
+	Article blogpost.ArticleServiceClient
+}
+
+func NewGrpcClients(cfg config.Config) (*GrpcClients, error) {
+	connAuthor, err := grpc.Dial(cfg.AuthorServiceGrpcHost+cfg.AuthorServiceGrpcPort, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	author := blogpost.NewAuthorServiceClient(connAuthor)
+
+	connArticle, err := grpc.Dial(cfg.ArticleServiceGrpcHost+cfg.ArticleServiceGrpcPort, grpc.WithInsecure())
+	if err != nil {
+		return nil, err
+	}
+	article := blogpost.NewArticleServiceClient(connArticle)
+
+	return &GrpcClients{
+		Author:  author,
+		Article: article,
+	}, nil
+}
